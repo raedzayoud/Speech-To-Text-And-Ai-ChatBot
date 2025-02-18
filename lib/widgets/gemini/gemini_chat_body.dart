894d06dde0cd0ebfa4d7602c4ff39025dd84fb69
Raed <raed.zayoud.23@gmail.com>
2025-02-18 16:18:25 +0100

    Add some code to generate gemini

--- lib/widgets/gemini/gemini_chat_body.dart
+++ lib/widgets/gemini/gemini_chat_body.dart
@@ -1,5 +1,6 @@
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 class GeminiChatBody extends StatefulWidget {
   const GeminiChatBody({super.key});
@@ -9,18 +10,40 @@ class GeminiChatBody extends StatefulWidget {
 }
 
 class _GeminiChatBodyState extends State<GeminiChatBody> {
-  List<ChatMessage> message = [];
+  List<ChatMessage> Listmessage = [];
+  final Gemini gemini = Gemini.instance;
+  ChatUser currentUser = ChatUser(id: "0", firstName: "user");
+  ChatUser geminiUser = ChatUser(
+      id: "1",
+      firstName: "Gemini");
+      // profileImage:
+      //     "https://upload.wikimedia.org/wikipedia/commons/8/8a/Google_Gemini_logo.svg");
   @override
   Widget build(BuildContext context) {
-    ChatUser currentUser = ChatUser(id: "0", firstName: "user");
-    ChatUser geminiUser = ChatUser(
-        id: "1",
-        firstName: "Gemini",
-        profileImage:
-            "https://upload.wikimedia.org/wikipedia/commons/8/8a/Google_Gemini_logo.svg");
     return DashChat(
-        currentUser: currentUser, onSend: _sendMessage, messages: message);
+        currentUser: currentUser, onSend: _sendMessage, messages: Listmessage);
   }
 
-  void _sendMessage(ChatMessage chatMessage) {}
+  void _sendMessage(ChatMessage chatMessage) {
+    setState(() {
+      Listmessage.insert(0, chatMessage);
+    });
+    try {
+      String question = chatMessage.text;
+      gemini.streamGenerateContent(question).listen((event) {
+        ChatMessage? lastmessage = Listmessage.firstOrNull;
+        if (lastmessage != null && lastmessage.user == geminiUser) {
+        } else {
+          String response = event.content?.parts
+                  ?.fold("", (previous, current) => "${previous}${current}") ??
+              "";
+          ChatMessage message =
+              ChatMessage(user: geminiUser, createdAt: DateTime.now(),text: response);
+              setState(() {
+                Listmessage.insert(0, message);
+              });
+        }
+      });
+    } catch (e) {}
+  }
 }
